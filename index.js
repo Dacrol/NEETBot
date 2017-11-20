992d19b4688a9c6572b62c6c8238e6f583c39af9
Dacrol <jonas.ahrens@gmail.com>
2017-11-20 22:25:05 +0100

    Fix scripts

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const { Composer, log } = require('micro-bot')
+const { Composer } = require('micro-bot')
+const { log } = require('micro-bot')
 
 const bot = new Composer()
 
 bot.use(log())
+
 bot.command('start', ({ reply }) => reply('Hey!'))
 
 module.exports = bot
